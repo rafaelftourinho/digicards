b418a199a699fd539da3d7b73ab04ba36a12d803
Rafael França <rafaelftourinho@gmail.com>
2022-07-08 20:47:55 -0300

    :recycle: refatorando a função

--- api.js
+++ api.js
@@ -3,28 +3,28 @@ const imgDigi = document.querySelector('#img-digi');
 const digiTitle = document.querySelector('.card-title');
 const digiText = document.querySelector('.card-text');
 
-const fetchAPI = () => 'https://digimon-api.vercel.app/api/digimon';
+const fetchAPI = async () => {
+  const response = await fetch('https://digimon-api.vercel.app/api/digimon');
+  const data = await response.json();
+  return data;
+}
 
-function digiAPI(url) {
+function digiAPI() {
 
   input.addEventListener('change', async ({ target }) => {
-    const digi = await fetch(url);
-    const digiArr = await digi.json();
-    const find = digiArr.find(({ name }) => name.toLowerCase() === target.value.toLowerCase());
-    const image = find.img;
+    const digiArr = await fetchAPI();
 
+    const find = digiArr.find(({ name }) => name.toLowerCase() === target.value.toLowerCase());
+
+    const image = find.img;
     const { name, level } = find;
 
     digiText.innerText = `${name} é um digimon do tipo ${level}`;
     digiTitle.innerHTML = name;
     imgDigi.src = image;
-    console.log(imgDigi);
 
-    console.log(find);
     return find;
   })
 }
 
-window.onload = () => digiAPI(fetchAPI());
-
-module.exports = { digiAPI };
+window.onload = () => digiAPI();
